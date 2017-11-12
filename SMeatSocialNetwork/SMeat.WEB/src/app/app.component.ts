985d39cb558429812e5c06d049a9c3a59684b3a5
FatalIdiot <vmanmaslov@gmail.com>
2017-11-13 00:33:43 +0200

    Atempted to add jQuery and worked on main page

--- SMeatSocialNetwork/SMeat.WEB/src/app/app.component.ts
+++ SMeatSocialNetwork/SMeat.WEB/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import * as jQuery from 'jquery';
 
 import { AppService } from './app.service';
 
