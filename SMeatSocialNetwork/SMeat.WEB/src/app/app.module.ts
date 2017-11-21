1471326224a40f9f1ab494db3efc85955eadbf20
валентин <valuta777@gmail.com>
2017-11-22 01:30:17 +0200

    Migrate to angular 5, Add minor style fixes

--- SMeatSocialNetwork/SMeat.WEB/src/app/app.module.ts
+++ SMeatSocialNetwork/SMeat.WEB/src/app/app.module.ts
@@ -4,8 +4,10 @@ import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSidenavModule} from '@angular/material/sidenav';
-
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 
@@ -76,7 +78,10 @@ const appRoutes: Routes = [
     HttpModule,
 	
 	BrowserAnimationsModule,
-	MatSidenavModule
+	MatSidenavModule,
+	MatButtonModule,
+	MatIconModule,
+	AngularFontAwesomeModule
   ],
   providers: [
       AppService,
